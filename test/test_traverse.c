c058fba4bff78763d843136bbed3d6e0a21afe02
Clay Dowling <clay@lazarusid.com>
2016-11-25 12:50:26 -0500

    Tests of traverse for my own comfort

--- test/test_traverse.c
+++ test/test_traverse.c
@@ -38,6 +38,60 @@ START_TEST(astTraversePostorder_givenThreeNodes_visitsLeftRightParent)
 }
 END_TEST
 
+START_TEST(astTraversePostOrder_givenDeeperLeft_visitsDeepLeftFirst)
+{
+    struct ast *a;
+    struct ast *b;
+    struct ast *c;
+    struct ast *plus;
+    struct ast *minus;
+
+    a = ast_create('A');
+    b = ast_create('B');
+    c = ast_create('C');
+    plus = ast_create('+');
+    minus = ast_create('-');
+
+    ast_attach_left(minus, a);
+    ast_attach_right(minus, b);
+    ast_attach_left(plus, minus);
+    ast_attach_right(plus, c);
+
+    ast_traverse_postorder(plus, print_node, NULL);
+
+    ck_assert_str_eq(buffer, "AB-C+");
+
+    ast_release(plus);
+}
+END_TEST
+
+START_TEST(astTraversePostOrder_givenDeeperRight_visitsDeepLeftFirst)
+    {
+        struct ast *a;
+        struct ast *b;
+        struct ast *c;
+        struct ast *plus;
+        struct ast *minus;
+
+        a = ast_create('A');
+        b = ast_create('B');
+        c = ast_create('C');
+        plus = ast_create('+');
+        minus = ast_create('-');
+
+        ast_attach_left(minus, a);
+        ast_attach_right(minus, b);
+        ast_attach_left(plus, c);
+        ast_attach_right(plus, minus);
+
+        ast_traverse_postorder(plus, print_node, NULL);
+
+        ck_assert_str_eq(buffer, "CAB-+");
+
+        ast_release(plus);
+    }
+END_TEST
+
 TCase *tcase_traverse(void)
 {
     TCase *tc;
@@ -45,6 +99,8 @@ TCase *tcase_traverse(void)
     tc = tcase_create("traversal");
     tcase_add_checked_fixture(tc, setup, NULL);
     tcase_add_test(tc, astTraversePostorder_givenThreeNodes_visitsLeftRightParent);
+    tcase_add_test(tc, astTraversePostOrder_givenDeeperLeft_visitsDeepLeftFirst);
+    tcase_add_test(tc, astTraversePostOrder_givenDeeperRight_visitsDeepLeftFirst);
 
     return tc;
 }
