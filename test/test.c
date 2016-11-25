3d43a815e7fbd7b0265430222bb1729182c686d0
Clay Dowling <clay@lazarusid.com>
2016-11-25 08:21:31 -0500

    Added ability to run tests with cmake
    To facilitate doing development with CLion, I have duplicated the
    functionality of the Makefile with CMake.

--- test/test.c
+++ test/test.c
@@ -11,11 +11,19 @@ int main(int argc, char **argv)
 	Suite *s;
 	SRunner *runner;
 	int number_fails;
+    int forkme = 1;
+
+    if (argc > 1 && strcmp(argv[1], "--nofork") == 0) {
+        forkme = 0;
+    }
 
 	s = suite_ast();
 	runner = srunner_create(s);
 	srunner_add_suite(runner, suite_stack());
 
+    if (0 == forkme) {
+        srunner_set_fork_status(runner, CK_NOFORK);
+    }
 	srunner_run_all(runner, CK_NORMAL);
 	number_fails = srunner_ntests_failed(runner);
 
